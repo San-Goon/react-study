4d9223583a294d9168cf5f165d030210d81d11a7
San-Goon <tigerlove95@naver.com>
2022-08-09 23:26:38 +0900

    feat: studied Class/Functional Component's state and LifeCycle

---
+++ src/components/2-5.State/FunctionalComponent.jsx
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from "react";
+
+const FunctionalComponent = () => {
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const timerID = setInterval(()=>tick(),1000);
+        return () => {
+            clearInterval(timerID)
+        }
+    }, [])
+
+    const tick = () => {
+        setDate(new Date());
+    }
+
+    return (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {date.toLocaleTimeString()}.</h2>
+        </div>
+    );
+}
+
+export default FunctionalComponent;
