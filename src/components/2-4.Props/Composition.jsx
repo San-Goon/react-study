499d519fbe9c05a7713aa7274ac7de11fe58ce9d
San-Goon <tigerlove95@naver.com>
2022-08-09 22:38:54 +0900

    feat: studied props, component extraction.

--- src/components/2-4.Props/Composition.jsx
+++ src/components/2-4.Props/Composition.jsx
@@ -1,5 +1,16 @@
 import React from "react"
 
-export default Composition () => {
-
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>
 }
+
+const Composition = () => {
+    return <div>
+        <Welcome name={"Jimmy"} />
+        <Welcome name={"Timmy"} />
+        <Welcome name={"Tommy"} />
+        <Welcome name={"Amy"} />
+    </div>
+}
+
+export default Composition;
---
+++ src/components/2-4.Props/Extraction/Extraction.jsx
@@ -0,0 +1,59 @@
+import React from "react"
+
+function formatDate(date) {
+    return date.toLocaleDateString();
+}
+
+function Avatar(props) {
+    return (
+        <img className="Avatar"
+         src={props.user.avatarUrl}
+         alt={props.user.name} />
+    )
+}
+
+const UserInfo = (props) => {
+    return (
+        <div className="UserInfo">
+            <Avatar user={props.author}/>
+            <div className="UserInfo-name">
+                {props.author.name}
+            </div>
+        </div>
+    )
+}
+
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <UserInfo author={props.author} />
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
+        </div>
+    );
+}
+
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+        name: 'Hello Kitty',
+        avatarUrl: 'http://placekitten.com/g/64/64'
+    }
+};
+
+
+const Extraction = () => {
+    return (
+        <Comment
+            date={comment.date}
+            text={comment.text}
+            author={comment.author} />
+    )
+}
+
+export default Extraction;
